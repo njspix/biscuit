843fc06534be0e5aaaed9db76dd0be33f3493cb2
Jacob Morrison <Jacob.Morrison@vai.org>
2026-07-21 13:12:00 -0400

    add MIT license to postalt.c

--- src/postalt.c
+++ src/postalt.c
@@ -1,5 +1,4 @@
-/**
- * biscuit postalt -- prefer primary chromosomes over unplaced/alt contigs.
+/* biscuit postalt -- prefer primary chromosomes over unplaced/alt contigs.
  *
  * Reads an aligned SAM/BAM. For each primary record, if its own contig is an
  * unplaced/unlocalized/alt contig (--unplaced mode: names not matching the
@@ -16,7 +15,28 @@
  * fixed value (a clean unique hit is a true 60 anyway; downstream masking only
  * tests MAPQ thresholds).
  *
+ * The MIT License (MIT)
+ *
  * Copyright (c) 2026 Wanding.Zhou@pennmedicine.upenn.edu
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
  */
 #include <unistd.h>
 #include <getopt.h>
